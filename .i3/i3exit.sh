17f66efc616edff939db1a9d98abe5c71df7f208
Keith Goodman <kwgoodman@gmail.com>
2016-11-06 08:36:29 -0800

    update suspend etc in i3wm for ubuntu 16.04

--- .i3/i3exit.sh
+++ .i3/i3exit.sh
@@ -8,18 +8,20 @@ case "$1" in
         i3-msg exit
         ;;
     suspend)
-        # sleep is to avoid race condition with suspend
         i3lock -c000000 && sleep 1 && \
-        dbus-send --system --print-reply --dest="org.freedesktop.UPower" \
-        /org/freedesktop/UPower org.freedesktop.UPower.Suspend
+        dbus-send --system --print-reply --dest=org.freedesktop.login1 \
+        /org/freedesktop/login1 "org.freedesktop.login1.Manager.Suspend" \
+        boolean:true
         ;;
     reboot)
-        dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" \
-        /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Restart
+        dbus-send --system --print-reply --dest=org.freedesktop.login1 \
+        /org/freedesktop/login1 "org.freedesktop.login1.Manager.Reboot" \
+        boolean:true
         ;;
     shutdown)
-        dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" \
-        /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop
+        dbus-send --system --print-reply --dest=org.freedesktop.login1 \
+        /org/freedesktop/login1 "org.freedesktop.login1.Manager.PowerOff" \
+        boolean:true
         ;;
     *)
         echo "Usage: $0 {lock|logout|suspend|reboot|shutdown}"
